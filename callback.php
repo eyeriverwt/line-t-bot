6c546987162e8f442ffc1f3d3c4a978d72235235
ai.kawakami <ai.kawakami@widetec.com>
2018-05-25 12:57:25 +0900

    second commit

--- callback.php
+++ callback.php
@@ -1,125 +1,51 @@
 <?php
-$accessToken = 'Line Developersで発行したアクセストークン';
+//require_once dirname(__FILE__) . '/config.php';
+//require_once dirname(__FILE__) . '/response_format_text.php';
+//require_once dirname(__FILE__) . '/curl.php';
+//require_once dirname(__FILE__) . '/register.php';
 
-$jsonString = file_get_contents('php://input');
-error_log($jsonString);
-$jsonObj = json_decode($jsonString);
 
-$message = $jsonObj->{"events"}[0]->{"message"};
-$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+//APIから送信されてきたイベントオブジェクトを取得
+$json_string = file_get_contents('php://input');
+$json_obj = json_decode($json_string);
 
-// 送られてきたメッセージの中身からレスポンスのタイプを選択
-if ($message->{"text"} == '確認') {
-    // 確認ダイアログタイプ
-    $messageData = [
-        'type' => 'template',
-        'altText' => '確認ダイアログ',
-        'template' => [
-            'type' => 'confirm',
-            'text' => '元気ですかー？',
-            'actions' => [
-                [
-                    'type' => 'message',
-                    'label' => '元気です',
-                    'text' => '元気です'
-                ],
-                [
-                    'type' => 'message',
-                    'label' => 'まあまあです',
-                    'text' => 'まあまあです'
-                ],
-            ]
-        ]
-    ];
-} elseif ($message->{"text"} == 'ボタン') {
-    // ボタンタイプ
-    $messageData = [
-        'type' => 'template',
-        'altText' => 'ボタン',
-        'template' => [
-            'type' => 'buttons',
-            'title' => 'タイトルです',
-            'text' => '選択してね',
-            'actions' => [
-                [
-                    'type' => 'postback',
-                    'label' => 'webhookにpost送信',
-                    'data' => 'value'
-                ],
-                [
-                    'type' => 'uri',
-                    'label' => 'googleへ移動',
-                    'uri' => 'https://google.com'
-                ]
-            ]
-        ]
-    ];
-} elseif ($message->{"text"} == 'カルーセル') {
-    // カルーセルタイプ
-    $messageData = [
-        'type' => 'template',
-        'altText' => 'カルーセル',
-        'template' => [
-            'type' => 'carousel',
-            'columns' => [
-                [
-                    'title' => 'カルーセル1',
-                    'text' => 'カルーセル1です',
-                    'actions' => [
-                        [
-                            'type' => 'postback',
-                            'label' => 'webhookにpost送信',
-                            'data' => 'value'
-                        ],
-                        [
-                            'type' => 'uri',
-                            'label' => '美容の口コミ広場を見る',
-                            'uri' => 'http://clinic.e-kuchikomi.info/'
-                        ]
-                    ]
-                ],
-                [
-                    'title' => 'カルーセル2',
-                    'text' => 'カルーセル2です',
-                    'actions' => [
-                        [
-                            'type' => 'postback',
-                            'label' => 'webhookにpost送信',
-                            'data' => 'value'
-                        ],
-                        [
-                            'type' => 'uri',
-                            'label' => '女美会を見る',
-                            'uri' => 'https://jobikai.com/'
-                        ]
-                    ]
-                ],
-            ]
-        ]
-    ];
-} else {
-    // それ以外は送られてきたテキストをオウム返し
-    $messageData = [
-        'type' => 'text',
-        'text' => $message->{"text"}
-    ];
-}
+//イベントオブジェクトから必要な情報を抽出
+$source = $json_obj->{"events"}[0]->{"source"};
+$message = $json_obj->{"events"}[0]->{"message"};
+$reply_token = $json_obj->{"events"}[0]->{"replyToken"};
 
-$response = [
-    'replyToken' => $replyToken,
-    'messages' => [$messageData]
+// group_idの取得
+$group_id =  $source->{"groupId"};
+
+$space_ignored = str_replace(" ", "", $message->{"text"} );
+$exploded = explode(",", $space_ignored);
+
+$access_token = '4TTcwP8p15f7nzWgUziMkI0XzqUZBtZE4AoIoyrdiMYyGHF6hF4p658x5LdMuFOxdFqnAs/vvtBsRNqhco8xDzquLSsWdOADJ1Y9AE3yYaQIDmany4AqgwQpEYXukHqqZxkpQmGGBJ7kDoC049aS3AdB04t89/1O/w1cDnyilFU=';
+
+$response_format_text = [
+    "type" => "text",
+    "text" => "おはよう！今日も１日頑張ってね〜！"
 ];
-error_log(json_encode($response));
 
-$ch = curl_init('https://api.line.me/v2/bot/message/reply');
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . $accessToken
-));
-$result = curl_exec($ch);
-error_log($result);
-curl_close($ch);
+
+$post_data = [
+    "replyToken" => $reply_token,
+    "messages" => [$response_format_text]
+];
+
+curl($post_data, $access_token);
+
+function curl($post_data, $access_token) {
+    //curlを使用してメッセージを返信する
+    $ch = curl_init("https://api.line.me/v2/bot/message/reply");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charser=UTF-8',
+        'Authorization: Bearer ' . $access_token
+    ));
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
