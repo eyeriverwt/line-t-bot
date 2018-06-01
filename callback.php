5d516d8402c2f0a5b5d4068dd34c07a885f6c70d
ai.kawakami <ai.kawakami@widetec.com>
2018-06-01 14:11:51 +0900

    2

--- callback.php
+++ callback.php
@@ -4,7 +4,6 @@ require_once dirname(__FILE__) . '/config.php';
 //require_once dirname(__FILE__) . '/curl.php';
 //require_once dirname(__FILE__) . '/register.php';
 
-
 //APIから送信されてきたイベントオブジェクトを取得
 $json_string = file_get_contents('php://input');
 $json_obj = json_decode($json_string);
@@ -20,18 +19,85 @@ $reply_token = $json_obj->{"events"}[0]->{"replyToken"};
 $space_ignored = str_replace(" ", "", $message->{"text"} );
 $exploded = explode(",", $space_ignored);
 
-$response_format_text = [
-    'type' => 'text',
-    'text' => 'testttt'
-];
+//CSVデータを読み込む
+$csv_filepath = 'list.csv';
+try {
+    $data["list"] = array_csv($csv_filepath);
+} catch (Exception $e) {
+    echo "error：", $e->getMessage(), "\n";
+}
+
+//search
+$flg = 0;
+foreach ((array)$data['list'] as $key => $value) {
+    if ($value[1] == $message->{"text"}) {
+        $bottext = $value[0] ."の技で" .$value[2] ."です。";
+        $flg = 1;
+        break;
+    }
+}
+
+// 送られてきたメッセージからレスポンスのタイプを選択
+if ($flg == 1) {
+    $response_format_text = [
+        'type' => 'text',
+        'text' => $bottext
+    ];
+} elseif($message->{"text"} == 'スタンプ'){
+    $response_format_text = [
+        'type' => 'sticker',
+        'packageId' => 2,
+        'stickerId' => 1
+    ];
+}else{
+    // それ以外は送られてきたテキストをオウム返し
+    $response_format_text = [
+        'type' => 'text',
+        //'text' => $message->{"text"}
+        'text' => 'ちょっとわかんないです...'
+    ];
+}
 $post_data = [
     "replyToken" => $reply_token,
-    "messages" => $response_format_text
+    "messages" => [$response_format_text]
 ];
-//send
+$response_format_text2 = [
+    'type' => 'text',
+    'text' => '2行目のテキストです。'
+];
+$post_data2 = [
+    "replyToken" => $reply_token,
+    "messages" => [$response_format_text2]
+];
 curl($post_data, $access_token);
+curl($post_data2, $access_token);
 
 
+
+//@return array $csv_data csv配列
+function array_csv($csv_filepath) {
+    //TODO エラー処理
+    setlocale(LC_ALL, 'ja_JP.UTF-8');
+    $handle = file_get_contents($csv_filepath);
+    //$handle = mb_convert_encoding($handle, 'UTF-8', 'sjis-win');
+    $temp = tmpfile();
+    $csv_data  = array();
+    fwrite($temp, $handle);
+    rewind($temp);
+    while (($handle = fgetcsv($temp, 0, ",")) !== false) {
+        $tmp_value = array();
+        foreach ($handle as $key1 => $value1) {
+            $tmp_value[] = trim ($value1);
+        }
+        $csv_data[] = $tmp_value;
+        //$tempstr[] = implode(",",$handle);//配列を区切り文字で文字列化
+    }
+    $message = ERR_CSV_EMPTY .$csv_filepath;
+    if(empty($csv_data)) {
+        throw new Exception($message);
+    }
+    return $csv_data;
+}
 
 
 function curl($post_data, $access_token) {
